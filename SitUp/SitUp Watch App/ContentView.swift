c45f38b08dc32a84cbb742b40ccda00ea7ee0586
Maryam <107400570+Neilah7@users.noreply.github.com>
2022-11-24 14:40:45 +0300

    Add files via upload

---
+++ SitUp/SitUp Watch App/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  SitUp Watch App
+//
+//  Created by maryam almijlad on 23/11/2022.
+//
+
+import SwiftUI
+//import HealthKit
+//add sound, vibrobites
+// preumime features(streaches)
+struct SitUp: View {
+    var body: some View {
+        
+        ScrollView(.vertical){
+           
+            
+            VStack {
+               
+                   
+                
+                Text("Hello, SitUp app!")
+              
+                
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        SitUp()
+    }
+}
---
+++ SitUp/SitUp Watch App/SitUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SitUpApp.swift
+//  SitUp Watch App
+//
+//  Created by maryam almijlad on 23/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SitUp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+           SitUp()
+        }
+    }
+}
